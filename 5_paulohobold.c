bb3912d84d1cb5f1112311473c0b26039c7e1472
PauloHoboldGIT <paulo.hobold@catolicasc.edu.br>
2025-03-14 05:14:33 -0300

    Add files via upload

---
+++ 5_paulohobold.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+  int num_1,num_2,soma;
+  
+printf("Digite um numero: ");
+  scanf("%d", &num_1);
+  
+printf("Digite o segundo numero: ");
+  scanf("%d", &num_2);
+
+  soma = num_1 + num_2;
+  
+printf("A soma dos dois numeros e: %d", soma);  
+
+      
+  return 0;
+  
+}
+
+
+//Desenvolva um programa que solicite dois n´umeros ao usu´ario, calcule a soma entre
+//eles e mostre o resultado da soma.
