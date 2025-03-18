0e6336b75f1bf308e9472cc642ee453cd4254ba8
PauloHoboldGIT <paulo.hobold@catolicasc.edu.br>
2025-03-18 10:58:27 -0300

    Add files via upload

---
+++ a2_paulohobld/1_paulohobold.c
@@ -0,0 +1,17 @@
+  #include <stdio.h>
+
+  int main() {
+    int n1;
+
+    printf("Digite um numero: ");
+    scanf("%d",&n1);
+
+    if (n1 <0){
+        printf("Numero negativo\n");
+    }else{
+      printf("Numero positivo\n");
+    }
+
+ return 0;
+  }
+//1. Fa¸ca um programa que solicite um n´umero e diga se ele ´e: Positivo ou Negativo.
