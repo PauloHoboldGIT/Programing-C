129a38c574a7e55181a2be467360e8ad968730ac
PauloHoboldGIT <paulo.hobold@catolicasc.edu.br>
2025-03-18 11:29:19 -0300

    Add files via upload

---
+++ a2_paulohobld/2_paulohobold.c
@@ -0,0 +1,18 @@
+  #include <stdio.h>
+
+  int main() {
+    int n1;
+
+    printf("Digite um numero: ");
+    scanf("%d",&n1);
+
+    if(n1%2==0){
+      printf("Numero par");
+    }
+    else{
+      printf("Numero impar");
+    }
+
+ return 0;
+  }
+//2-Fa¸ca um programa que solicite um n´umero e diga se ele ´e: Par ou ´ımpar
